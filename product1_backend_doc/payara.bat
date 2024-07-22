ca42e2703daacc38959e312db66ee138d0cf5242
Marek Ladicky <mla@digitalsystems.eu>
2024-07-22 21:47:43 +0200

    update to java 17

--- product1_backend_doc/payara.bat
+++ product1_backend_doc/payara.bat
@@ -1,8 +1,8 @@
 rmdir /s /q pmroot
-C:/devel/jdk-11.0.19/bin/java.exe ^
+C:/devel/jdk-17.0.12/bin/java.exe ^
 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8088 ^
 -DconfigFilePath=app_config.properties ^
--jar C:\devel\mavenrepo\fish\payara\extras\payara-micro\6.2023.7\payara-micro-6.2023.7.jar ^
+-jar C:\devel\mavenrepo\fish\payara\extras\payara-micro\6.2024.7\payara-micro-6.2024.7.jar ^
 --deploy ../product1_backend/product1_rest/target/restapp.war ^
 --port 8081 ^
 --rootDir pmroot ^
